9548a3a5ade1f019525ff8d2e99a2cc34c47a92e
SALI MU <sally791009@gmail.com>
2020-05-08 19:21:00 +0800

    calculate

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open ('reviews.txt', 'r')as f:
 		count += 1
 		if count % 1000 ==0: # %是餘數
 			print(len(data))
-print(len(data))
-print(data[0]) #在框框裡的指令會一直loop，記得要往外退
-print('---------!!!---------')
-print(data[1])
-		
+print('檔案讀取完囉，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每筆留言平均長度為', sum_len/len(data))
+
