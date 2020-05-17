90b6bc736e3b2ed93c9cc10f8975a2b3c2539928
SALI MU <sally791009@gmail.com>
2020-05-17 12:17:43 +0800

    add dictionary

--- read.py
+++ read.py
@@ -20,3 +20,43 @@ for d in data:
 print('一共有', len(new), '筆長度小於100')
 print(new[0])
 print(new[5])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[69])
+
+
+bad = ['bad' in d for d in data ]
+print(bad)
+
+#文字計數
+wc = {}
+for d in data:
+	words = d.split() #split預設值是空白鍵
+	for word in words: 
+		if word in wc:
+			wc[word]+= 1
+		else:
+			wc[word] = 1 #新增新的key進入字典
+
+for word in wc: #把單字一字一行的印出來
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc)) #知道有幾個key
+
+print(wc['Sally'])
+
+while True:
+	word = input('請輸入你想查的字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現的次數是：', wc[word])
+
+	else:
+		print('這個字沒有出現過')
+	
+print('謝謝您使用此功能')
